5ee0f0dc86e8f6096d0d4fde64ffeacf11b50917
Jeremias Knoblauch <knoblauc@treepipit.stats.ox.ac.uk>
2017-01-21 17:10:18 +0000

    Prototype BN-layer added now as nn_bn_layer function. The function permits double input and works. It can be used in exactly the same way as nn_layer, including the parameter sequence supplied as input. It does permit one additional parameter epsilon, but that has a default value of 0.01 and should not be changed. (Epsilon is a numerical stability guarantee to make sure that the variance is never estimated to be zero)

--- NNBuilder.py
+++ NNBuilder.py
@@ -12,7 +12,9 @@ def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return initial
   
-def bias_variable(shape):
+def bias_variable(shape): 
+	#the batch mean. Instead, the role of the bias is performed
+	# by the new beta variable. See Section 3.2 of the BN2015 paper.
     initial = tf.constant(0.0, shape=shape)
     return tf.Variable(initial)
   
@@ -38,12 +40,53 @@ def nn_layer(input_tensor, input_dim, output_dim, layer_name, double_input = Non
                 preactivate2 = tf.matmul(double_input, weights) + biases
             else: preactivate2 = None
     return preactivate, preactivate2, weights, biases
+    
+
+#NOTE: epsilon makes sure that the variance is not estimated to be zero and is added to the variance estimation
+#
+def nn_bn_layer(input_tensor, input_dim, output_dim, layer_name, double_input = None, epsilon=0.01):
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            weights = tf.Variable(xavier_init([input_dim, output_dim]))
+            variable_summaries(weights)
+        with tf.name_scope('biases'):
+            biases = bias_variable([output_dim])
+            variable_summaries(biases)
+        with tf.name_scope('Wx_plus_b'):
+            preactivate = tf.matmul(input_tensor, weights) + biases
+            batch_mean1, batch_var1 = tf.nn.moments(preactivate,[0])	#the second argument is simply a speci-
+																							#fication of how moments are calculated     
+            scale = tf.Variable(tf.ones([output_dim]))		#the scale variable (gamma) that is also learnt in BN
+            beta = tf.Variable(tf.zeros([output_dim]))		#the beta variable that is also learnt in BN
+             
+			   #if you need double input, compute batch mean and variance over both inputs
+            if double_input != None:
+                preactivate2 = tf.matmul(double_input, weights) + biases
+                batch_mean2, batch_var2 = tf.nn.moments(preactivate2,[0])
+                batch_mean = 0.5*(batch_mean1 + batch_mean2)		#get overall mean
+                batch_var = (input_dim-1.0)/(2.0*input_dim - 1.0) * (batch_var1 + batch_var2)	#get overall variance   
+                preactivate2 = tf.nn.batch_normalization(preactivate2,batch_mean,batch_var,beta,scale,epsilon)   
+            #if you don't need double input, compute batch mean and batch variance only over preactivate1
+            else: 
+                preactivate2 = None     
+            	 #ADD BN only for preactivate 1!
+                batch_mean, batch_var = batch_mean1, batch_var1 #so the mean/var from first batch is used exclusively
+					 
+            preactivate = tf.nn.batch_normalization(preactivate,batch_mean,batch_var,beta,scale,epsilon)
+            
+            #lastly, add summary writers to the preactivations
+            variable_summaries(preactivate)           
+            tf.summary.histogram('pre_activations', preactivate)
+        return preactivate, preactivate2, weights, biases
+    
+    
+
          
   #----------Models-Copied from blogpost in slack, should give atleast a reasonable result------------------------------------------
   
   #Generator NN: z -> (100,128) -> reLU -> (128,784) -> Sigmoid
 def Generator_NN(input, input_dim, output_dim):
-    hidden_1, _, w1, b1 = nn_layer(input, input_dim, 128, 'layer1_G')
+    hidden_1, _, w1, b1 = nn_b_layer(input, input_dim, 128, 'layer1_G')				
     hidden_2, _, w2, b2 = nn_layer(tf.nn.relu(hidden_1), 128, output_dim, 'layer2_G')
     return tf.nn.sigmoid(hidden_2),[w1,w2,b1,b2]
   
